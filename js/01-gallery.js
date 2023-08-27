fb65a85f4094a766b014b18326a9f86b6b2af9ee
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-27 21:32:11 +0200

    hw7-task1-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(
+    galleryItem => `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src="${galleryItem.original}"
+alt="${galleryItem.description}"></a></li>`
+  )
+  .join('');
+imgList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+imgList.addEventListener('click', onClick);
+
+function onClick(e) {
+  if (e.target.nodeName === 'IMG') {
+    const instance = basicLightbox.create(`
+    <img src="${e.target.src}" width="800" height="600">
+`);
+
+    instance.show();
+  }
+}
